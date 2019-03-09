6e1b0bef06e00d83ba7fbd5c048b45460a0b5512
Cheloved <penguin@localhost.localdomain>
2019-03-09 18:05:31 +0300

    Initial

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+void MainWindow::RefreshWidgetList(QObject* parent)
+{
+    this->widgetList.insert( std::make_pair( parent->objectName(), parent ) );
+    QList<QObject*> children = parent->children();
+    for ( QObject* child : children )
+        RefreshWidgetList(child);
+}
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    RefreshWidgetList(this);
 }
 
 MainWindow::~MainWindow()
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <map>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    void RefreshWidgetList(QObject* parent);
+    std::map<QString, QObject*> widgetList;
 };
 
 #endif // MAINWINDOW_H
