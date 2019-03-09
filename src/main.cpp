2a484dceab782b7865f3a3db1452dc03e1fd3ec8
Cheloved <27046990+Cheloved@users.noreply.github.com>
2019-03-09 21:01:57 +0300

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,11 @@ int main(int argc, char *argv[])
     //MainWindow w;
     //w.show();
 
+    // Media player and it's holder
     QMediaPlayer* player = new QMediaPlayer;
     QVideoWidget* videoWidget = new QVideoWidget;
 
+    // Get screen resolution
     QScreen* screen = QGuiApplication::primaryScreen();
     QRect rect = screen->geometry();
 
@@ -25,10 +27,13 @@ int main(int argc, char *argv[])
     int width = 600;
     int height = 400;
 
+    // Bind media player to widget
     player->setVideoOutput(videoWidget);
+
+    // Default built-in playback
     player->setMedia(QUrl::fromLocalFile("/home/penguin/Downloads/videoplayback"));
 
-
+    // Install default geometry
     videoWidget->setGeometry(screenWidth/2-width/2,screenHeight/2-height/2,width,height);
 
     videoWidget->show();
